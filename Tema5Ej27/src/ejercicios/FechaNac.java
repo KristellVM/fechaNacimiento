a684b003bf2a7dd8ad2945a38a878302c8fad8b9
KristellVM <krisvm31@gmail.com>
2024-11-06 12:16:22 +0100

    programa completo

--- Tema5Ej27/src/ejercicios/FechaNac.java
+++ Tema5Ej27/src/ejercicios/FechaNac.java
@@ -1,5 +1,9 @@
 package ejercicios;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 import util.Funciones;
@@ -9,6 +13,10 @@ public class FechaNac {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int opcion = 0;
+		LocalDate hoy = LocalDate.now();
+		LocalDate fecha;
+		fecha = Funciones.dimeFecha("Dime tu fecha de nacimiento", sc);
+
 		do {
 			System.out.println("Selecciona la operación:");
 			System.out.println("1. Calcular edad en días");
@@ -20,12 +28,26 @@ public class FechaNac {
 			
 			switch(opcion) {
 			case 1:
+				System.out.println("Tu edad en días son: "+ChronoUnit.DAYS.between(fecha,hoy)+" días");
 				break;
 			case 2:
+				System.out.println("Tu edad en meses son: "+ChronoUnit.MONTHS.between(fecha, hoy)+" meses");
 				break;
 			case 3:
+				Period edad = Period.between(fecha, hoy);
+				System.out.println("Tu edad completa es: "+edad.getYears()+" años, "+edad.getMonths()+" meses y "+edad.getDays()+" días");
 				break;
 			case 4:
+				LocalDate fecha2 = LocalDate.of(hoy.getYear(), fecha.getMonth(), fecha.getDayOfMonth());
+				LocalDate futuro = LocalDate.of(hoy.getYear()+1, fecha.getMonth(), fecha.getDayOfMonth());
+				LocalDate mismoAño = LocalDate.of(hoy.getYear(),fecha.getMonth(),fecha.getDayOfMonth());
+				if(fecha2.isBefore(hoy)) {
+					System.out.println("Los días que faltan para tu próximo cumpleaños son: "+ChronoUnit.DAYS.between(hoy, futuro));
+				} else if(fecha2.isAfter(hoy)) {
+					System.out.println("Los días que faltan para tu próximo cumpleaños son: "+ChronoUnit.DAYS.between(hoy, mismoAño));
+				} else {
+					System.out.println("Tu cumpleaños es hoy!");
+				}
 				break;
 			case 0:
 				System.out.println("Terminó la operación");
